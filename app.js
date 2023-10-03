268bc349e8bb4e1c1eaf26cbad1eeb0f7bcbe6b5
OsamaTaani <nottaani@gmail.com>
2023-10-03 21:18:47 +0300

    pushing the Dom task in new branch

--- app.js
+++ app.js
@@ -1,74 +1,7 @@
-// function orderInputs(){
-//     const name = prompt("Enter your name : ");
-
-
-  
-//     let gender;
-//     while (true){
-//         gender = prompt("Enter your gender (Male/Female): ");
-
-//         if (gender.toLowerCase() ==="male"){
-
-//             alert(`Welcome , Mr. ${name}`);
-//             break;
-//         }
-//         else if(gender.toLocaleLowerCase() === "female"){
-//             alert(`Welcome , Ms. ${name}`);
-//             break;
-//         }
-//         else{
-//             alert("Please enter valid gender .");
-//         }
-//     }
-
-//     // if (gender.toLocaleLowerCase() === "male"){
-//     //      alertMessage = `Welcome , Mr. ${name}!`;
-//     // }
-//     // else if (gender.toLocaleLowerCase() === "female"){
-//     //     alertMessage = `Welcome , Ms. ${name}!`;
-//     // }
-//     // else {
-//     //     alertMessage = `Welcome , ${name}!`;
-//     // }
-//     // alert(alertMessage);
-
-//     const drinkTemp = prompt("Would you like (Hot/Cold) drink ? ");
-
-//     const drinkName = prompt("What drink would you like to order ? ");
-
-//     alert(`Your ${drinkTemp} ${drinkName} is getting prepared , pleaase be patient (: `);
-
-//     const arr1 = [`${name}` , `${gender}` , `${drinkTemp}` , `${drinkName}` ];
-//     console.log(`${arr1}`);
-//     for (let i = 0; i < arr1.length; i++){
-//         console.log(arr1[i]);
-//     }
-    
-
-//     console.log(`${name} has ordered a ${drinkTemp} ${drinkName}`);
-
-
-// }
-// document.getElementById("startOrderButton").addEventListener("click",orderInputs);
-
-function getGender(name) {
-    let gender;
-    while (true) {
-        gender = prompt("Enter your gender (Male/Female): ");
-
-        if (gender.toLowerCase() === "male") {
-            return `Mr. ${name}`;
-        } else if (gender.toLowerCase() === "female") {
-            return `Ms. ${name}`;
-        } else {
-            alert("Please enter a valid gender.");
-        }
-    }
-}
-
 function orderInputs() {
+
     const name = prompt("Enter your name: ");
-    const greeting = getGender(name);
+    const {greeting , gender} = getGender(name);
     alert(`Welcome, ${greeting}`);
 
     const drinkTemp = prompt("Would you like (Hot/Cold) drink?");
@@ -80,6 +13,40 @@ function orderInputs() {
     console.log(arr1);
 
     console.log(`${name} has ordered a ${drinkTemp} ${drinkName}`);
+
+    
+
+    let div = document.getElementById("userInfos");
+    let p = document.createElement("p");
+    let ulElements = document.createElement("ul");
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    let li3 = document.createElement("li");
+
+
+    p.textContent = `${name}`;
+    li1.textContent = `Gender: ${gender}`;
+    li2.textContent = `Drink : ${drinkName} ${drinkTemp}`;
+
+    div.appendChild(p);
+    div.appendChild(ulElements);
+    ulElements.appendChild(li1);
+    ulElements.appendChild(li2);
+
+}
+function getGender(name) {
+    let gender;
+    while (true) {
+        gender = prompt("Enter your gender (Male/Female): ");
+
+        if (gender.toLowerCase() === "male") {
+            return { greeting: `Mr. ${name}`, gender };
+        } else if (gender.toLowerCase() === "female") {
+            return { greeting: `Ms. ${name}`, gender };
+        } else {
+            alert("Please enter a valid gender.");
+        }
+    }
 }
 
 document.getElementById("startOrderButton").addEventListener("click", orderInputs);
